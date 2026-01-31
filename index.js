a00b906000ecf23ae152e06a88f6f2798b765c97
Andres Martin <acmartinn94@gmail.com>
2026-01-30 23:43:15 -0500

    creado buscador de cockteles

--- index.js
+++ index.js
@@ -21,6 +21,22 @@ app.get("/", async (req, res) => {
     }   
 }); 
 
+app.get("/search", async (req, res) => {
+  const bebida = req.query.bebida;
+  try {
+    const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${bebida}`);
+    let drinks = response.data.drinks;
+    console.log(drinks.length);
+    res.render("index.ejs", { drinks });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+        error: error.message,
+    });
+  }
+});
+
+
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
