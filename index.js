c9b5b02f66ab9e7742e80dd4d9d702046e8b62ba
Andres Martin <acmartinn94@gmail.com>
2026-02-01 01:45:46 -0500

    proyecto final para mejoras

--- index.js
+++ index.js
@@ -20,13 +20,73 @@ app.get("/", async (req, res) => {
     });
     }   
 }); 
-
+app.get("/drink-details", async (req, res) => {
+  const drinkid = req.query.id;
+  
+  try {
+    const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkid}`);
+    let drinks = response.data.drinks;
+    
+    res.render("description.ejs",{drinks});
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("description.ejs", {           
+        error: error.message,
+    });
+    } 
+}); 
+app.get("/drink-glosary", async (req, res) => {
+  const letter = req.query.letter;
+  console.log(letter)
+  
+  try {
+    const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`);
+    let drinks = response.data.drinks;
+    
+    res.render("index.ejs",{drinks});
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {           
+        error: error.message,
+    });
+    } 
+}); 
+app.get("/drink-random", async (req, res) => {
+  
+  
+  try {
+    const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/random.php`);
+    let drinks = response.data.drinks;
+    
+    res.render("description.ejs",{drinks});
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("description.ejs", {           
+        error: error.message,
+    });
+    } 
+}); 
+app.get("/drink-random", async (req, res) => {
+  
+  
+  try {
+    const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/random.php`);
+    let drinks = response.data.drinks;
+    
+    res.render("description.ejs",{drinks});
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("description.ejs", {           
+        error: error.message,
+    });
+    } 
+}); 
 app.get("/search", async (req, res) => {
   const bebida = req.query.bebida;
   try {
     const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${bebida}`);
     let drinks = response.data.drinks;
-    console.log(drinks.length);
+    
     res.render("index.ejs", { drinks });
   } catch (error) {
     console.error("Failed to make request:", error.message);
